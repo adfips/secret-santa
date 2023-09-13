114e997578fdaa1370907a155baaf314c7f11913
Fenya2 <dim74.09@mail.ru>
2023-09-13 11:43:36 +0500

    first commit

---
+++ src/test/java/pidagogi/secret/santa/SecretSantaApplicationTests.java
@@ -0,0 +1,13 @@
+package pidagogi.secret.santa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
