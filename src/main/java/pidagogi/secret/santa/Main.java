36ba54a6699ddcf2af29f4f5a07071493b02525f
Fenya2 <dim74.09@mail.ru>
2023-09-14 12:03:57 +0500

    simple web-service from javarush tutorial

--- src/main/java/pidagogi/secret/santa/Main.java
+++ src/main/java/pidagogi/secret/santa/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecretSantaApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecretSantaApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
-
 }
+
